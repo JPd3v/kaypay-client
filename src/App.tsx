97c1ec2f75329c95e5fdbc465be8e4dc5dc2e2bb
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-09-25 04:24:28 -0300

    adds react-query and router dom config

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
-import { useState } from 'react';
-import reactLogo from 'assets/react.svg';
-// import viteLogo from './vite.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="bg-red-200">
-        <a href="https://vitejs.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" /> */}
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <label>
-        <input type="text" />
-      </label>
-
-      <h1 className="bg-slate-500 text-yellow-300 text-7xl">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<div>placeholder</div>} />
+    </Routes>
   );
 }
 
