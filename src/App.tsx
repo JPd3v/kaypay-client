308155835f72771852ce51d112be43c040ddf614
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-10-01 03:37:14 -0300

    adds basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css';
+import { MainLayout } from 'components';
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+const Home = lazy(() => import('pages/Home'));
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div>placeholder</div>} />
-    </Routes>
+    <Suspense fallback={<MainLayout />}>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<div>page not found</div>} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
