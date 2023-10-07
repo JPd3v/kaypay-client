678313956d4e1416d182bb6116dad331640b3a47
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-10-07 05:57:27 -0300

    fix modals that do not overflow when the height is
    too large for the screen

--- src/features/modals/components/ModalWrapper.tsx
+++ src/features/modals/components/ModalWrapper.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 export default function ModalWrapper({ children }: Props) {
   return (
-    <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-gray-900/20">
-      <ReactFocusLock className="flex w-full justify-center">
+    <div className="fixed left-0 top-0 z-50 flex h-screen w-screen overflow-auto bg-gray-900/20">
+      <ReactFocusLock className="m-auto flex w-full justify-center">
         {children}
       </ReactFocusLock>
     </div>
