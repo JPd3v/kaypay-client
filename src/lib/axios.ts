2f01c952bbc9d9c8cafddeb50c8480a30cc9f1ab
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-10-12 05:21:28 -0300

    adds axios interceptors for refresh user tokens
    if the response is 401
    -adds withCredentials into axios instance config

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,16 @@
-import Axios from 'axios';
+import Axios, { AxiosError } from 'axios';
 import { API_URL } from 'config';
+import { refreshTokens } from 'features/auth';
 
-const axios = Axios.create({ baseURL: API_URL });
+const axios = Axios.create({ baseURL: API_URL, withCredentials: true });
+axios.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      await refreshTokens();
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default axios;
