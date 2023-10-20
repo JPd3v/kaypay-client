537d6480d08d49115e43753c44c1abccc3388c66
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-10-20 05:55:05 -0300

    fix interceptors bug

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,15 @@
-import Axios, { AxiosError } from 'axios';
+import Axios from 'axios';
 import { API_URL } from 'config';
 import { refreshTokens } from 'features/auth';
 
 const axios = Axios.create({ baseURL: API_URL, withCredentials: true });
 axios.interceptors.response.use(
   (response) => response,
-  async (error: AxiosError) => {
+  async (error) => {
+    const originalRequest = error.config;
     if (error.response?.status === 401) {
       await refreshTokens();
+      return axios(originalRequest);
     }
     return Promise.reject(error);
   },
